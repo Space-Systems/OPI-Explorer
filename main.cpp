0159b5d680c426992bf943cc33af468a21f97b92
Marek Moeckel <marekmoeckel@serc.org.au>
2021-02-11 09:17:37 +1100

    Adjust the global font size

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     OpiExplorerMain w;
+    QFont globalFont = w.font();
+    globalFont.setPointSize(10);
+    w.setFont(globalFont);
     w.show();
 
     return a.exec();
